45df6f877b849a3630534ff566109d413d10d205
unknown <christian.collaguazom@gmail.com>
2021-03-09 00:50:29 -0500

    Antes de Ng Prime

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+/**
+ * * Aquí se importan los modelos locales a ser utilizados en el proyecto.
+*/
+import { AppRoutingModule } from './app.routing';
+
+/**
+ * * Aquí se importan los componentes a ser utilizados en el proyecto.
+*/
+import { LoginComponent } from './view/auth/login/login.component';
+import { MainComponent } from './view/main/main/main.component';
+import { LayoutComponent } from './view/layout/layout/layout.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LayoutComponent,
+    MainComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
